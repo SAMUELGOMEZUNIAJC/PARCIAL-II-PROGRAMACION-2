4901c37fa9925deaf8be6fd5865e9b62c3b0d498
SAMUELGOMEZUNIAJC <148736186+SAMUELGOMEZUNIAJC@users.noreply.github.com>
2024-10-16 19:08:54 -0500

    Create Grading.java

---
+++ Grading.java
@@ -0,0 +1,4 @@
+public interface Grading {
+    void calculateFinalGrade(); // Método para calcular la nota final
+    void getGrades(); // Método para obtener las calificaciones de los estudiantes
+}
